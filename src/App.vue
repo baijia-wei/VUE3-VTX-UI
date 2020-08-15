354851e47801b941b8b3b620baeedab19c43f6dd
白家玮 <2443891115@qq.com>
2020-08-15 20:59:02 +0800

    路由初始化

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <div>hi</div>
-  <Frank msg="Hello Vue 3.0 + Vite" />
+  <div>
+    导航栏
+    <router-link to="/">frank</router-link>|
+    <router-link to="/xxx">frank2</router-link>
+  </div>
+
+  <hr />
+  <router-view />
 </template>
 
 <script>
-import Frank from "./components/Frank.vue";
-
 export default {
   name: "App",
-  components: {
-    Frank,
-  },
 };
 </script>
---
+++ src/components/Frank2.vue
@@ -0,0 +1,14 @@
+<template>
+<div>Frank2</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+</style>
