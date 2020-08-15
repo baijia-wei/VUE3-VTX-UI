46e54764177eb49cbf2aa4648ad4acd7dd4a5b98
白家玮 <2443891115@qq.com>
2020-08-15 19:31:29 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Frank msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import Frank from "./components/Frank.vue";
+
+export default {
+  name: "App",
+  components: {
+    Frank,
+  },
+};
+</script>
---
+++ src/components/Frank.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>我的第一个组件</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "",
+    };
+  },
+  created() {},
+  methods: {},
+  mounted() {},
+  computed: {},
+  watch: {},
+};
+</script>
+
+<style scoped>
+</style>
